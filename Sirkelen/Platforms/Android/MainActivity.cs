21e5c4a29c62aa5e929df75898f9f939f5c83951
Alexander Kvalvaag <alexanderkvalvaag@gmail.com>
2025-02-15 22:48:12 +0100

    Keyboard adjusting fixed

--- Sirkelen/Platforms/Android/MainActivity.cs
+++ Sirkelen/Platforms/Android/MainActivity.cs
@@ -1,10 +1,17 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Views;
 using Android.OS;
 
 namespace Sirkelen;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
-public class MainActivity : MauiAppCompatActivity
-{
-}
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Window.SetSoftInputMode(SoftInput.AdjustResize);
+        }
+    }
+
